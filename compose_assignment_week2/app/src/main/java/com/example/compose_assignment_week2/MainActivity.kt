0738a31c1c00194348fb87525ca05c4fe79a26e3
Yunseo <osope@naver.com>
2026-03-16 20:41:39 +0900

    Add Kotlin mobile computing projects

---
+++ compose_assignment_week2/app/src/main/java/com/example/compose_assignment_week2/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.compose_assignment_week2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose_assignment_week2.ui.theme.Compose_assignment_week2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Compose_assignment_week2Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeQuadrant()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeQuadrant() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+
+        }
+    }
+}
+
+@Composable
+private fun ComposableCard(
+    title: String,
+    description: String,
+    backgrouundColor: Color,
+    modifier: Modifier = Modifier
+)
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Compose_assignment_week2Theme {
+        Quadrant("Android")
+    }
+}
