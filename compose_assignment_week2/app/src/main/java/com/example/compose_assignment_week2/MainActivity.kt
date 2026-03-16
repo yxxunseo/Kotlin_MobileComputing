31bdbf8792259d689a997acefff0bc59df3ffa1c
Yunseo <osope@naver.com>
2026-03-16 21:01:43 +0900

    Fix Compose

--- compose_assignment_week2/app/src/main/java/com/example/compose_assignment_week2/MainActivity.kt
+++ compose_assignment_week2/app/src/main/java/com/example/compose_assignment_week2/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.compose_assignment_week2
 
+import android.R.attr.title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,9 +18,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_assignment_week2.ui.theme.Compose_assignment_week2Theme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +49,32 @@ class MainActivity : ComponentActivity() {
 fun ComposeQuadrant() {
     Column(Modifier.fillMaxWidth()) {
         Row(Modifier.weight(1f)) {
-
+            ComposableCard(
+                title = "Text composable",
+                description = "Displays text and follows the recommended Material Design guidelines.",
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableCard(
+                title = "Image composable",
+                description = "Creates a composable that lays out and draws a given Painter class object.",
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableCard(
+                title = "Row composable",
+                description = "A layout composable that places its children in a horizontal sequence.",
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableCard(
+                title = "Column composable",
+                description = "A layout composable that places its children in a vertical sequence.",
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
         }
     }
 }
@@ -49,14 +83,33 @@ fun ComposeQuadrant() {
 private fun ComposableCard(
     title: String,
     description: String,
-    backgrouundColor: Color,
+    backgroundColor: Color,
     modifier: Modifier = Modifier
-)
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom=16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_assignment_week2Theme {
-        Quadrant("Android")
+        ComposeQuadrant()
     }
 }
